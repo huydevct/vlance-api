114bf8de668b4757e3382713443584a8636a64a6
huydevct <huyctdev@gmail.com>
2022-12-30 11:18:12 +0700

    feat: chatting

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   /**
@@ -7,7 +7,7 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {},
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -16,5 +16,56 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap(/*{ strapi }*/) {
+    //strapi.server.httpServer is the new update for Strapi V4
+    var io = require("socket.io")(strapi.server.httpServer, {
+      cors: {
+        // cors setup
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true,
+      },
+    });
+    io.on("connection", function (socket) {
+      //Listening for a connection from the frontend
+      socket.on("join", ({ username }) => {
+        // Listening for a join connection
+        console.log("user connected");
+        console.log("username is ", username);
+        if (username) {
+          socket.join("group"); // Adding the user to the group
+          socket.emit("welcome", {
+            // Sending a welcome message to the User
+            user: "bot",
+            text: `${username}, Welcome to the group chat`,
+            userData: username,
+          });
+        } else {
+          console.log("An error occurred");
+        }
+      });
+      socket.on("sendMessage", async (data) => {
+        // Listening for a sendMessage connection
+        let strapiData = {
+          // Generating the message data to be stored in Strapi
+          data: {
+            user: data.user,
+            message: data.message,
+          },
+        };
+        var axios = require("axios");
+        await axios
+          .post("http://localhost:1337/api/messages", strapiData) //Storing the messages in Strapi
+          .then((e) => {
+            socket.broadcast.to("group").emit("message", {
+              //Sending the message to the group
+              user: data.username,
+              text: data.message,
+            });
+          })
+          .catch((e) => console.log("error", e.message));
+      });
+    });
+  },
 };
